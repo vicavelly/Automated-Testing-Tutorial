687644d020b301890a076fe97a892cd782db8a0d
Emmanuel Vitus <emmanuelvitus01@gmail.com>
2023-05-28 09:26:55 -0700

    Initial commit

---
+++ Tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automated_Testing_Tutorial
+{
+    class Tutorial
+    {
+
+       // Automated_Testing
+       /*
+            Automated testing is the practice of writing codes to test our code, and then run those test
+            in an automated fashion
+
+            With automated testing, our cods/program will consist of production codes and test codes
+
+            BENEFITS OF AUTOMATED TESTING
+            -----------------------------
+            1.  Ability to test your application code frequently and in less time.\
+            2.  The main advantage is the ability to catch bugs in the application before deployment. 
+                NOTE: This doesn't mean our programs would be completely bug free, but this can certainly reduce the num
+                        of bugs and improve the quality of our software.
+
+            3.  Refactoring with confidence.  Without automated testing, Whenever we refactor our code, we have to manually 
+                                              test every part of the application that could be affected by our refactoring
+
+            4.  It helps us focus more on the quality of code / quality of methods that we're writing.
+        */
+    }
+}
